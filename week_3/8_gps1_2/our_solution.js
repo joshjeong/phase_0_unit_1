38bb69ac077fdc626dfa8be4325d2f3495114295
joshjeong <jcjeong@gmail.com>
2014-04-17 14:55:21 -0700

    Add Solutions to GPS

--- week_3/8_gps1_2/our_solution.js
+++ week_3/8_gps1_2/our_solution.js
@@ -1,22 +1,30 @@
 // GPS 1.2 - JavaScript
 
 // YOUR FULL NAMES:
-//  1.
-//  2.
+//  1. Stella Kim
+//  2. Josh Jeong
 
 
 // 1. "YOU SIGNED... WHO?!"
 
-
-
+function Client(name, age, quote)
+{
+    this.name = name;
+    this.age = age;
+    this.quote = quote;
+}
 
 // 2. "Here they Come!"
 
+var adamSandler = new Client("Adam Sandler", 47, "That's your home! Are you too good for your home?!");
+var kristenBell = new Client("Kristen Bell", 33, "Do you wanna build a snowman?");
+var jimCarrey = new Client("Jim Carrey", 52, "...So you're telling me there's a chance? YEAH!");
 
-
-
+var clientList = [adamSandler, kristenBell, jimCarrey];
 
 // 3. "TIME IS MONEY!"
+
+
 
 //YOUR CODE HERE!
 
@@ -26,13 +34,14 @@ shooterMcGavin.age === 48;
 shooterMcGavin.quote === "Just stay out of my way... or you'll pay. Listen to what I say.";
 
 
-
-
-
 // 4. "SHOW 'EM OFF!"
 
-
-
+for(var i=0; i<clientList.length; i++) {  
+  console.log(i+1+".");
+  console.log("   "+ clientList[i].name);
+  console.log("   "+ clientList[i].age);
+  console.log("   "+ clientList[i].quote);
+}
 
 // ** BONUS **
 
