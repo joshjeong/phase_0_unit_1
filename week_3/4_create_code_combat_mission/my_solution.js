4ee15f504e676d9f783a0e03c1c3c7971a3dfa42
joshjeong <jcjeong@gmail.com>
2014-04-20 10:56:08 -0700

    Update my_solution.js

--- week_3/4_create_code_combat_mission/my_solution.js
+++ week_3/4_create_code_combat_mission/my_solution.js
@@ -1,28 +1,45 @@
-// I worked [by myself, with:] on this challenge
+// I worked with Matt on this challenge
 
-// Your mission description:
-// 
-// 
-// 
+/* Instructions:
+1. Define local variables in JavaScript
+2. Create, add properties to, delete properties from, and access values from JavaScript Object literals
+
+
+/* Your mission description:
+
+
 
 // Pseudocode
-// 
-// 
-// 
-// 
-// 
-
+// Define character with properties age, name, and gender.
+// Quickly add new characteristics such hair color, eye color, and weapon.
+// Delete gender characteristics.
+*/
 // Initial Code
 
+var characterList= []
+
+function character(age, name, gender) {
+  this.age= age;
+  this.name= name;
+  this.gender= gender;
+}
+
+
+var hulk= new character( 35, "Hulk", "male")
+characterList.push(hulk)
+var captainAmerica= new character( 103, "Captain America", "male")
+characterList.push(captainAmerica)
+var superMan= new character( 41, "Super Man", "male")
+characterList.push(superMan)
 
 
 
-
-
-// Refactored Code
-
-
-
+superMan.hairColor= "brown";
+superMan.eyeColor= "brown";
+hulk.weapon= "Hulk Smash";
+delete superMan.gender;
+console.log(superMan)
+    
 
 
 
