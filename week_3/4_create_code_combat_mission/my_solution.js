f1eb0f5ce82ae0bdd943aac848415111c95d9262
joshjeong <jcjeong@gmail.com>
2014-04-21 13:17:06 -0700

    Update my_solution.js

--- week_3/4_create_code_combat_mission/my_solution.js
+++ week_3/4_create_code_combat_mission/my_solution.js
@@ -44,18 +44,18 @@ console.log(superMan)
 
 
 // Reflection
-What parts of your strategy worked? What problems did you face?
-    - I had some trouble understanding the directions. After doing code combat, I initially though it had to be 
-    extremely advanced.
-What questions did you have while coding? What resources did you find to help you answer them?
-    - I didn't know how to set up the constructor but it was later clarified after doing the GPS.
-What concepts are you having trouble with, or did you just figure something out? If so, what?
-    - I'm still having a little trouble with Objects but nothing that can't be answered after some additional reading.
-Did you learn any new skills or tricks?
-    - .push
-How confident are you with each of the learning objectives?
-    - Meh, as I mentioned before. I need to spend more time on Objects
-Which parts of the challenge did you enjoy?
-    - I did this in a pairing session which are always fun.
-Which parts of the challenge did you find tedious?
-    - None.
+// What parts of your strategy worked? What problems did you face?
+//     - I had some trouble understanding the directions. After doing code combat, I initially though it had to be 
+//     extremely advanced.
+// What questions did you have while coding? What resources did you find to help you answer them?
+//     - I didn't know how to set up the constructor but it was later clarified after doing the GPS.
+// What concepts are you having trouble with, or did you just figure something out? If so, what?
+//     - I'm still having a little trouble with Objects but nothing that can't be answered after some additional reading.
+// Did you learn any new skills or tricks?
+//     - .push
+// How confident are you with each of the learning objectives?
+//     - Meh, as I mentioned before. I need to spend more time on Objects
+// Which parts of the challenge did you enjoy?
+//     - I did this in a pairing session which are always fun.
+// Which parts of the challenge did you find tedious?
+//     - None.
