126449e4d416de9c991abb4c0d6226f3a2523d2c
joshjeong <jcjeong@gmail.com>
2014-04-21 13:31:47 -0700

    Update gc1_initial_solution.js

--- week_3/5_group_project/gc1_initial_solution.js
+++ week_3/5_group_project/gc1_initial_solution.js
@@ -79,3 +79,24 @@ assert(
 )
 
 "group.js" 98L, 1959C
+
+// Reflection
+// What parts of your strategy worked? What problems did you face?
+// 	- We were able to communicate with each other very well. We faced a hiccup with our median code, but we 
+// 	caught it and were able to fix it.
+// What concepts are you having trouble with, or did you just figure something out? If so, what?
+// 	- My section was converting the pseudo to code which wasn't too difficult
+// Did you learn any new skills or tricks?
+// 	- None
+// How confident are you with each of the learning objectives?
+// 	- Ok
+// Which parts of the challenge did you enjoy?
+// 	- Working as a group to complete a challenge.
+// Which parts of the challenge did you find tedious?
+// 	- None
+// What was this experience like?
+// 	- It was different. Taking someone's work where they left off was a fun task.
+// Was your group successful in passing the tests?
+// 	- After we fixed our median error, we were able to pass the test. 
+// What did you learn about writing user stories, pseudocoding, writing code, refactoring, etc.?
+// 	- That is helpful/important to plan out your strategy before diving in and checking work after.
